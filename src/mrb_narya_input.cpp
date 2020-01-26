850e56ab43b934d22a58119017e9fbb83e7eea61
kishima <kishima7@gmail.com>
2020-01-26 20:54:24 +0900

    fix keyboard issue

--- src/mrb_narya_input.cpp
+++ src/mrb_narya_input.cpp
@@ -14,23 +14,31 @@
 #include "fabgl.h"
 #include "fabutils.h"
 
+#include "fmruby.h"
+
 extern fabgl::PS2Controller PS2Controller;
 
 MRB_BEGIN_DECL
 mrb_value mrb_narya_input_available(mrb_state *mrb, mrb_value self)
 {
   auto keyboard = PS2Controller.keyboard();
-  bool ret = keyboard->isKeyboardAvailable();
+  bool ret = keyboard->virtualKeyAvailable();
   return ret ? mrb_true_value() : mrb_false_value();
 }
 
 mrb_value mrb_narya_input_keydown(mrb_state *mrb, mrb_value self)
 {
   auto keyboard = PS2Controller.keyboard();
-  if(keyboard->isKeyboardAvailable()){
+
+  if(keyboard->virtualKeyAvailable()){
     mrb_int vkey;
     mrb_get_args(mrb, "i", &vkey);
+
     bool ret = keyboard->isVKDown((fabgl::VirtualKey)vkey);
+    if(ret){
+      keyboard->emptyVirtualKeyQueue();
+    }
+    //FMRB_DEBUG(FMRB_LOG::DEBUG,"kdwn:%d:%d\n",vkey,ret);
     return ret ? mrb_true_value() : mrb_false_value();
   }
   return mrb_false_value();
